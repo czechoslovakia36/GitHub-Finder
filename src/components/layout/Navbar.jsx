94fe7f7ff0d2731c255ce6c6245111cdd0146eb5
Vivek Kumar <55905538+czechoslovakia36@users.noreply.github.com>
2022-11-22 14:14:48 +0530

    footer component added

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,7 +6,10 @@ import PropTypes from 'prop-types'
 
 
 function Navbar() {
+
   return (
+
+    
     <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content' >
         <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
