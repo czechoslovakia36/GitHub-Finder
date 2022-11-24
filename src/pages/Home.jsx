6e676bbb473d9e917a896e793c66bd68a0622b49
Vivek Kumar <55905538+czechoslovakia36@users.noreply.github.com>
2022-11-24 10:44:16 +0530

    getting user data with fetch api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import UserResults from '../components/users/UserResults'
 function Home() {
   return (
     <div>
-        {process.env.REACT_APP_GITHUB_TOKEN}
-        
+        {/* {process.env.REACT_APP_GITHUB_TOKEN} */}
+        <>
+        {/* SEARCH COMPONENT */}
+          <UserResults/>
+        </>
     </div>
     )
 }
